fcab839de18ca128aa11d3a4c19cdb3b03f95eb4
BRUNO HENRIQUE DOS SANTOS <bruno.h.santos@outlook.com>
2018-08-24 15:53:13 -0300

    sync

--- entrypoint/config-db.sh
+++ entrypoint/config-db.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-sudo pg_dropcluster --stop 9.5 main
+pg_dropcluster --stop 9.5 main
 
-sudo pg_createcluster -u postgres -g postgres -e LATIN1 --locale="pt_BR.ISO-8859-1" --lc-collate="pt_BR.ISO-8859-1" 9.5 ecidade
+pg_createcluster -u postgres -g postgres -e LATIN1 --locale="pt_BR.ISO-8859-1" --lc-collate="pt_BR.ISO-8859-1" 9.5 ecidade
 
 cp /setup/postgresql.conf $PGDATA/postgresql.conf
 chown -R postgres.postgres $PGDATA/postgresql.conf
 
-sudo systemctl start postgresql.service
+systemctl start postgresql.service
 
 #/etc/init.d/postgresql restart
 #pg_createcluster -e LATIN1 9.5 main

e428c854405420eda35806c58059b2b39493e402
BRUNO HENRIQUE DOS SANTOS <bruno.h.santos@outlook.com>
2018-08-27 08:58:09 -0300

    sync

--- entrypoint/config-roles.sql
+++ entrypoint/config-roles.sql
@@ -2,17 +2,17 @@
 --DROP ROLE IF EXISTS dbseller;
 --DROP ROLE IF EXISTS plugin;
 
-IF NOT EXISTS (SELECT * FROM pg_user WHERE username = 'ecidade')
+--IF NOT EXISTS (SELECT * FROM pg_user WHERE username = 'ecidade')
 BEGIN
     create role ecidade with superuser login password 'ecidade';
 END;
 
-IF NOT EXISTS (SELECT * FROM pg_user WHERE username = 'dbseller')
+--IF NOT EXISTS (SELECT * FROM pg_user WHERE username = 'dbseller')
 BEGIN
     create role dbseller with login password 'dbseller';
 END;
 
-IF NOT EXISTS (SELECT * FROM pg_user WHERE username = 'plugin')
+--IF NOT EXISTS (SELECT * FROM pg_user WHERE username = 'plugin')
 BEGIN
     create role plugin with login password 'plugin';
 END;
